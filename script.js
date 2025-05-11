f679d6c3e2bfd72e1ae757d6d7fcc42f5faaa697
Adarsh Agrahari <115093974+abderaj17@users.noreply.github.com>
2025-05-11 21:40:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,16 @@
 var dateDiffInDays = function (date1, date2) {
   //   write your code here
+		const d1 = new Date(date1);
+		const d2 = new Date(date2);
+
+	const timeDiff = d2.getTime() - d1.getTime();
+
+	const dayDiff = timeDiff / (1000 * 60 * 60 * 24);
+
+	return dayDiff;
 };
 
 // Do not change the code below.
-const dateOne = prompt("Enter Start Date.");
-const dateTwo = prompt("Enter End Date.");
+// const dateOne = prompt("Enter Start Date.");
+// const dateTwo = prompt("Enter End Date.");
 alert(dateDiffInDays(dateOne, dateTwo));
